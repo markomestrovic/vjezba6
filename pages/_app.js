e172d1d6f0761d8b92b7b04077974c41429eb1e8
kula124 <kula124@gmail.com>
2022-11-01 17:46:37 +0100

    Vjezba 3: About CSS module

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import '../styles/about.css';
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
--- pages/about.js
+++ pages/about.js
@@ -1,23 +1,25 @@
 import Image from 'next/image';
 import HeaderFooterLayout from '../layouts/HeaderFooterLayout';
 
+import styles from '/styles/about.module.css';
+
 const About = () => (
     <HeaderFooterLayout>
-        <main className="container">
-            <section className="description">
-                <h1 className="title">About</h1>
-                <p className="text">
+        <main className={styles.container}>
+            <section className={styles.description}>
+                <h1 className={styles.title}>About</h1>
+                <p className={styles.text}>
                     We are software developers, designers, and entrepreneurs who are
                     passionate about building products that make a difference in
                     peoples lives. We are a team of 5 people based in the United
                     States and Canada. We use React and NextJS to build our web
                     applications.
                 </p>
-                <button className="learn-more-button">
+                <button className={styles['learn-more-button']}>
                     <a href="https://nextjs.org/">Learn more</a>
                 </button>
             </section>
-            <section className="image">
+            <section className={styles.image}>
                 <Image
                     width="100%"
                     height="100%"
