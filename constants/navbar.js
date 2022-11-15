cc3d2ca0b79aec8e12c74cfda843127177f52298
kula124 <kula124@gmail.com>
2022-11-15 17:53:29 +0100

    Vjezba 5: Init

--- constants/navbar.js
+++ constants/navbar.js
@@ -3,6 +3,6 @@ export const navigationItems = [
     { label: 'About us', path: '/about' },
     { label: 'Showcase', path: '' },
     { label: 'Blog', path: '' },
-    { label: 'Contact', path: '' },
-    { label: 'Sign in', path: '' },
+    { label: 'Sign in', path: '/login' },
+    { label: 'Me', path: '/self' },
 ];
---
+++ pages/login.js
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+
+import Spinner from '../components/Spinner';
+
+import styles from '../styles/login.module.scss';
+
+const users = [
+    {
+        id: 1,
+        username: 'admin',
+        password: 'admin',
+        name: 'Admin',
+        email: 'admin@example.com',
+    },
+    {
+        id: 2,
+        username: 'user',
+        password: 'user',
+        name: 'User',
+        email: 'user@example.com',
+    },
+];
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <main className={styles.page}>
+            <section className={styles.content}>
+                <h1 className={styles.title}>Login</h1>
+                <section className={styles.form}>
+                    <div className={styles.inputWrapper}>
+                        <input
+                            value={email}
+                            type="email"
+                            id="email"
+                            className={styles.emailInput}
+                            placeholder="Email or username"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className={styles.inputWrapper}>
+                        <input
+                            value={password}
+                            type="password"
+                            id="password"
+                            placeholder="Password"
+                            className={styles.passwordInput}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    {loading ? (
+                        <Spinner />
+                    ) : (
+                        <button
+                            onClick={handleSubmit}
+                            className={styles.submitButton}
+                        >
+                            Login
+                        </button>
+                    )}
+                </section>
+                {error && <p className={styles.error}>{error}</p>}
+            </section>
+        </main>
+    );
+};
+
+export default Login;
