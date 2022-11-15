8b04f2d3f98895cd3ac946b34a4119ce96ffe5d3
kula124 <kula124@gmail.com>
2022-11-15 21:57:25 +0100

    Vjezba 5: Actually store loggedIn state [WIP]

--- pages/login.js
+++ pages/login.js
@@ -27,6 +27,9 @@ const Login = () => {
 
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(
+        localStorage.getItem('isLoggedIn') === 'true'
+    );
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -41,6 +44,8 @@ const Login = () => {
                 setError('');
                 setLoading(false);
                 alert('Login successful!');
+                setIsLoggedIn(true);
+                localStorage.setItem('isLoggedIn', true);
             } else {
                 setError('Invalid credentials');
                 setLoading(false);
@@ -51,39 +56,43 @@ const Login = () => {
     return (
         <main className={styles.page}>
             <section className={styles.content}>
-                <h1 className={styles.title}>Login</h1>
-                <section className={styles.form}>
-                    <div className={styles.inputWrapper}>
-                        <input
-                            value={email}
-                            type="email"
-                            id="email"
-                            className={styles.emailInput}
-                            placeholder="Email or username"
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className={styles.inputWrapper}>
-                        <input
-                            value={password}
-                            type="password"
-                            id="password"
-                            placeholder="Password"
-                            className={styles.passwordInput}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    {loading ? (
-                        <Spinner />
-                    ) : (
-                        <button
-                            onClick={handleSubmit}
-                            className={styles.submitButton}
-                        >
-                            Login
-                        </button>
-                    )}
-                </section>
+                <h1 className={styles.title}>
+                    {isLoggedIn ? 'You are logged in!' : 'Log in'}
+                </h1>
+                {!isLoggedIn && (
+                    <section className={styles.form}>
+                        <div className={styles.inputWrapper}>
+                            <input
+                                value={email}
+                                type="email"
+                                id="email"
+                                className={styles.emailInput}
+                                placeholder="Email or username"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className={styles.inputWrapper}>
+                            <input
+                                value={password}
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                                className={styles.passwordInput}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        {loading ? (
+                            <Spinner />
+                        ) : (
+                            <button
+                                onClick={handleSubmit}
+                                className={styles.submitButton}
+                            >
+                                Login
+                            </button>
+                        )}
+                    </section>
+                )}
                 {error && <p className={styles.error}>{error}</p>}
             </section>
         </main>
