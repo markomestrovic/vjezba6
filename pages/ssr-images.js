c198c2ea4cca69b8d36f13df02bb6ab560ed3016
markomestrovic <markomestar64@gmail.com>
2022-12-11 11:02:08 +0100

    Vjezba 6: List all assets page

--- pages/ssr-images.js
+++ pages/ssr-images.js
@@ -1,33 +1,13 @@
 import Image from 'next/image';
 
-const SSRImages = ({ images }) => {
-    return (
-        <div>
-            <h1 className="text-4xl" align="center">
-                SSR Images
-            </h1>
-            <ul>
-                {images.map((image) => (
-                    <li key={image}>
-                        <p align="center">{image}</p>
-                        <div
-                            style={{
-                                position: 'relative',
-                                height: '300px',
-                            }}
-                        >
-                            <Image
-                                src={'/vjezba6/' + image}
-                                alt={image}
-                                layout="fill"
-                                objectFit="contain"
-                            />
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
+export async function getStaticProps() {
+    const images = fs.readdirSync('public/vjezba6');
+
+    return {
+        props: {
+            images,
+        },
+    };
+}
 
 export default SSRImages;
