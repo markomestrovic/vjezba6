9ad97a31cf960315fcc1c7b017692b899c78a4fb
markomestrovic <markomestar64@gmail.com>
2022-12-11 11:17:40 +0100

    Vjezba 6: Dynamic pages: SSR

--- pages/blog/[someId].js
+++ pages/blog/[someId].js
@@ -1,14 +1,18 @@
-import { useRouter } from 'next/router';
-
-const SomeBlogPost = () => {
-    const router = useRouter();
-
+const SomeBlogPost = ({someId}) => {
     return (
         <h1 className="text-2xl">
             HI! You are on{' '}
-            <span className="text-red-500">{router.query.someId}</span>
+            <span className="text-red-500">{someId}</span>
         </h1>
     );
 };
 
 export default SomeBlogPost;
+
+export function getServerSideProps(context) {
+    return {
+        props: {
+            someId: context.params.someId,
+        },
+    };
+}
