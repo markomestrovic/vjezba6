e7c8fd59893fabaa3496e66adac870dbd8cce19e
markomestrovic <markomestar64@gmail.com>
2022-12-11 11:27:40 +0100

    Vjezba 6: Blog posts SSG

--- pages/blog/[someId].js
+++ pages/blog/[someId].js
@@ -1,32 +1,49 @@
-const SomeBlogPost = ({someId}) => {
-    return (
-        <h1 className="text-2xl">
-            HI! You are on{' '}
-            <span className="text-red-500">{someId}</span>
-        </h1>
-    );
-};
+const BlogPost = ({ post }) => (
+    <main className="w-2/3 mx-auto my-0 flex flex-col items-center">
+        <h1 className="text-center text-8xl text-gray-800">{post.title}</h1>
+        <h2 className="text-center text-5xl my-8 text-gray-600">
+            Some subtitle can go here
+        </h2>
+        <Image
+            className="mx-auto"
+            src={post.imgSrc}
+            alt="post image"
+            layout="fixed"
+            width={800}
+            height={400}
+        />
+        <p className="text-gray-800 text-xl mt-16">{post.body}...</p>
+        <p>Read more bellow</p>
+        <a href={post.link}>
+            <span>Learn more</span>
+        </a>
+    </main>
+);
 
-export default SomeBlogPost;
+export default BlogPost;
 
-export function getStaticPaths() {
+export async function getStaticPaths() {
+    const posts = await api.getPosts();
+
+    const paths = posts.map((post) => ({
+        params: {
+            someId: post.slug,
+        },
+    }));
+
     return {
-        paths: [
-            { params: { someId: '1' } },
-            { params: { someId: '2' } },
-            { params: { someId: '3' } },
-            { params: { someId: 'stop' } },
-            { params: { someId: 'hammer' } },
-            { params: { someId: 'time' } },
-        ],
+        paths,
         fallback: false,
     };
-}
+}   
 
-export function getStaticProps(context) {
+
+export async function getStaticProps({ params: { someId } }) {
+    const post = await api.getPostBySlug(someId);
+
     return {
         props: {
-            someId: context.params.someId,
+            post,
         },
     };
 }
